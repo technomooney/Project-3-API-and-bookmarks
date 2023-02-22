9543d8a076921d5f9c2728568580a2e38b49523e
Marty M <github@moontronic.com>
2023-02-22 13:42:43 -0600

    fixed the issie with the home route and the home funtion that caused a crash

--- ui.py
+++ ui.py
@@ -1,9 +1,10 @@
-from flask import Flask, template_rendered
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
 def home():
-    return template_rendered("index.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
