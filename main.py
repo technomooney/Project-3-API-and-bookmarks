e0f31651a8471a04515ed9698ca72ba0d4aa296a
Marty M <github@moontronic.com>
2023-02-22 14:06:25 -0600

    forgot to save when editing main.pu to use the new folder name for the ui module

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flaskapp.ui import app
+from web_ui.ui import app
 
 # running the webapp... this can be done after checks and setup if needed... just demonstrating it can be done here
 app.run()
