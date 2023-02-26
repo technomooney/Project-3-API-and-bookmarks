d48ac2e891d911e61d1d451b9f952df42d76a5af
= <nkpratt04@gmail.com>
2023-02-26 16:33:42 -0600

    Add function to retrieve response from API

--- web_api/landscape_photos/unsplash.py
+++ web_api/landscape_photos/unsplash.py
@@ -1,11 +1,24 @@
 import requests
 import os
+from pprint import pprint
 
 base_url = 'https://api.unsplash.com/search/photos?'
 key = os.environ.get('UNSPLASH_KEY')
 
 def main():
-    'PLACEHOLDER'
+    """PLACEHOLDER"""
+
+
+def get_image_response():
+    try:
+        query = {'query': 'national parks', 'page': 1, 'per_page': 10, 'orientation': 'landscape', 'client_id': key}
+        response = requests.get(base_url, params=query)
+        response.raise_for_status()
+        data = response.json()
+        return data, None
+    except Exception as ex:
+        print(ex) # TODO - Switch to log instead of print
+        return None, ex
 
 
 if __name__ == '__main__':
