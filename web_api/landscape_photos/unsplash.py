5fdc990b6e9bf53a12dcf808e58d2a6679405d17
= <nkpratt04@gmail.com>
2023-02-26 16:17:25 -0600

    Setup main function structure

--- web_api/landscape_photos/unsplash.py
+++ web_api/landscape_photos/unsplash.py
@@ -1,2 +1,12 @@
 import requests
 import os
+
+base_url = 'https://api.unsplash.com/search/photos?'
+key = os.environ.get('UNSPLASH_KEY')
+
+def main():
+    'PLACEHOLDER'
+
+
+if __name__ == '__main__':
+    main()
