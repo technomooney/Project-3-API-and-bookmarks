bb0d96819b70ee7abbe54a5da85e66e5b47bf137
Marty M <github@moontronic.com>
2023-03-10 22:40:13 -0700

    modified the variable used for the image data to be more descriptive.

--- web_ui/ui.py
+++ web_ui/ui.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", data=test_image_data)
+    return render_template("index.html", image_list=test_image_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
