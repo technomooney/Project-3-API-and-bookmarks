4a3b7badd31f099398fffa25a8eaffa2bf4bd66e
Marty M <github@moontronic.com>
2023-03-10 21:03:26 -0700

    made a basic template based on the preposed layout in the slack chanel. still working on tweeks for the carousel.

--- web_ui/ui.py
+++ web_ui/ui.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-
+test_image_data = ""
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", data=test_image_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
