2132080af1ef13930620facb998a93e5c320fad6
= <nkpratt04@gmail.com>
2023-03-14 16:19:33 -0500

    Create image file/class

---
+++ web_api/landscape_photos/image.py
@@ -0,0 +1,8 @@
+
+class Image():
+
+    def __init__(self) -> None:
+        self.image_url = None
+        self.creator_name = None
+        self.creator_link = None
+        self.description = None
