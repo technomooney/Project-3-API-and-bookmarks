d3dad8fdba2f83a0da5b1c9cb408547ae44dd48c
= <nkpratt04@gmail.com>
2023-03-16 21:15:24 -0500

    Add docstring and comments to get_image_response (api request) function

--- web_api/landscape_photos/unsplash.py
+++ web_api/landscape_photos/unsplash.py
@@ -7,15 +7,20 @@ key = os.environ.get('UNSPLASH_KEY')
 
 
 def get_image_response():
-    try:
+    """Query UnSplash API to retrieve 10 random images and information on the creator.
+    Search terms: National parks, landscape orientation, 1 page, 10 images per page.
+    This function will raise an exception if an error occurs
+    and return the json data if no errors occur."""
+    try: 
+        # Query will request images matching 'national parks'. It requests one page with 10 random images in landscape orientation using env. variable key
         query = {'query': 'national parks', 'page': 1, 'per_page': 10, 'orientation': 'landscape', 'client_id': key}
         response = requests.get(base_url, params=query)
-        response.raise_for_status()
+        response.raise_for_status() # Raise exception if a client or server error occurs
         data = response.json()
-        return data
+        return data # Return data in json format if no errors occured during request
     except Exception as ex:
         print(ex) # TODO - Switch to log instead of print
-        return ex
+        return ex # Return any exceptions to be handled outside the function
     
 
 def create_image_object():
