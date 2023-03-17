7e5a1939d87db05082803af0b6d7ad4d97f4ec26
= <nkpratt04@gmail.com>
2023-03-16 21:48:12 -0500

    Add __repr__ method to Image class

--- web_api/landscape_photos/image.py
+++ web_api/landscape_photos/image.py
@@ -1,8 +1,12 @@
 
 class Image():
 
-    def __init__(self) -> None:
-        self.image_url = None
-        self.creator_name = None
-        self.creator_link = None
-        self.description = None
+    def __init__(self, image_url, creator_name, creator_link, image_description) -> None:
+        self.image_url = image_url
+        self.creator_name = creator_name
+        self.creator_link = creator_link
+        self.image_description = image_description
+    
+
+    def __repr__(self) -> str:
+        return f'Image(\'{self.image_url}\', \'{self.creator_name}\', \'{self.creator_link}\', \'{self.image_description}\')'
