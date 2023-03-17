597a2bd0265c9f624944ecc74894e9413f9b3817
= <nkpratt04@gmail.com>
2023-03-16 21:50:52 -0500

    Docstring/comments for create_image_object function

--- web_api/landscape_photos/unsplash.py
+++ web_api/landscape_photos/unsplash.py
@@ -11,6 +11,7 @@ def get_image_response():
     Search terms: National parks, landscape orientation, 1 page, 10 images per page.
     This function will raise an exception if an error occurs
     and return the json data if no errors occur."""
+    
     try: 
         # Query will request images matching 'national parks'. It requests one page with 10 random images in landscape orientation using env. variable key
         query = {'query': 'national parks', 'page': 1, 'per_page': 10, 'orientation': 'landscape', 'client_id': key}
@@ -24,20 +25,22 @@ def get_image_response():
     
 
 def create_image_object():
+    """This function calls the get_image_response function
+    to be parsed into an image object. For each of the 10
+    images on the page, it collects the image URL, creator's name,
+    link to the creator's profile, and a description of the image.
+    Function returns a list of 10 image objects."""
 
-    response_data = get_image_response()
-    results = response_data.get('results')
-
-    images = []
+    results = get_image_response.get('results') # Retrieve API response and get necessary resultset
+    images = [] # Empty list to be filled with 10 image objects
     
-    for image in results:
-
-        image_url = image.get('urls')['regular']
+    for image in results: # Gather needed data from each image dictionary
+        image_url = image.get('urls')['regular'] 
         creator_name = image.get('user')['name']
         creator_link = image.get('user')['links']['self']
         image_description = image.get('description')
 
-        new_image = Image(image_url, creator_name, creator_link, image_description)
+        new_image = Image(image_url, creator_name, creator_link, image_description) # Create new image and add to list of image objects
         images.append(new_image)
         
         return images
