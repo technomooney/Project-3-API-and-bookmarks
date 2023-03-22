bd6eea43069287de7e3cc2c6da2b16c7951f784b
Jules <MCTC@email>
2023-03-21 19:20:28 -0500

    Added code to populate NPS table

--- data_storage/db_interaction.py
+++ data_storage/db_interaction.py
@@ -36,6 +36,20 @@ class Parks:
     def __getattr__(self, name):
         return getattr(self.instance, name)
     
+    def populate_nps_table(self, park):
+        # SQL statement to insert park data
+        insert_sql = '''INSERT INTO NPS
+                        (ParkCode, ParkName, Latitude, Longitude, ParkDescription, PhoneNum, Email)
+                        VALUES (?, ?, ?, ?, ?, ?, ?)'''
+
+        # execute SQL statement with park data
+        conn = sqlite3.connect(db)
+        with conn:
+            conn.execute(insert_sql, (self.park_code, park.full_name, park.location[0], park.location[1],
+                                      park.description, None, None))
+        conn.commit()
+        conn.close()
+    
     def populate_unsplash_table(self):
         # call create_image_object to retrieve list of image objects
         images = create_image_object()
@@ -73,6 +87,8 @@ class Parks:
                     conn.execute(insert_sql, (self.park_code, day, time, values[0], values[1], values[2], values[3]))
         conn.commit()
         conn.close()
+
+
     
 if __name__ == '__main__':
     Parks()
