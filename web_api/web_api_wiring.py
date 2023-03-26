255e876f1a7592d90887ae47912ee00c723fb673
= <nkpratt04@gmail.com>
2023-03-26 16:22:18 -0500

    Add function to call unsplash functions that will return a list of image objects

--- web_api/web_api_wiring.py
+++ web_api/web_api_wiring.py
@@ -2,3 +2,15 @@ from landscape_photos import unsplash
 from national_park import national_parks
 from open_weather import open_weather_map
 
+def get_unsplash_image_list():
+    """This function calls the API request function from unsplash.py.
+    The request function either returns a response or an exception;
+    if there's an error, it will be logged, else, the create image object
+    list function is called and the list of image objects is returned."""
+    response, error = unsplash.get_image_response() # Call unsplash function to get response or exception, depending on whether the request is successful
+
+    if error:
+        print(f'An error has occured: {error}') # TODO - switch to log instead of print
+    else: 
+        image_list = unsplash.create_image_object_list(response) # Get image list from unsplash function if request is successful
+        return image_list
