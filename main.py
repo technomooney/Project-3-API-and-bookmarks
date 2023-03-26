9ac608ff78e373e0d85c624a12e1838dd598b250
Marty M <github@moontronic.com>
2023-03-26 14:51:12 -0600

    removed old testing code from the ui branch)

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-from web_ui.ui import app
+from flask import Flask, render_template, session, redirect
+from scratch import test
+app = Flask(__name__)
 
-# running the webapp... this can be done after checks and setup if needed... just demonstrating it can be done here
-app.run()
 
+@app.route("/")
+def home():
+    return render_template("index.html", image_list=test_image_data)
+
+@app.route("/save", methods=['POST'])
+def save():
+    park = request.form.get('park_code')
+    include_weather = request.form.get('include_weather')
+    print(park, include_weather)
+
+
+@app.route("/display_saved_parks")
+def display_saved_parks():
+    return render_template('base.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
