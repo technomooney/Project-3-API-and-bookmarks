6a41a28fa29050bb50f8457194339df8ae8f85e7
Marty M <github@moontronic.com>
2023-03-26 13:51:30 -0600

    starting to implement sessions to allow for passing data between multiple pages.

--- web_ui/ui.py
+++ web_ui/ui.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,session, redirect
 from scratch import test
 test_image_data = ""
 app = Flask(__name__)
@@ -18,11 +18,16 @@ park_list=[yellowstone]
 def home():
     return render_template("index.html", image_list=test_image_data)
 
-@app.route("/current", methods=['GET','POST'])
-def live_page():
-    park = yellowstone
-    return render_template("current_data.html", park=park)
-    
+@app.route("/save", methods=['POST'])
+def save():
+    park = request.form.get('park_code')
+    include_weather = request.form.get('include_weather')
+    print(park, include_weather)
+
+
+@app.route("/display_saved_parks")
+def display_saved_parks():
+    return render_template('base.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
