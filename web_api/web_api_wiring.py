76ae769d38a000dffe8d512fb58cb94cd5c0b3ff
= <nkpratt04@gmail.com>
2023-03-26 16:36:45 -0500

    Made a spelling error - fixed the word 'occurred'

--- web_api/web_api_wiring.py
+++ web_api/web_api_wiring.py
@@ -10,7 +10,7 @@ def get_unsplash_image_list():
     response, error = unsplash.get_image_response() # Call unsplash function to get response or exception, depending on whether the request is successful
 
     if error:
-        print(f'An error has occured: {error}') # TODO - switch to log instead of print
+        print(f'An error has occurred: {error}') # TODO - switch to log instead of print
     else: 
         image_list = unsplash.create_image_object_list(response) # Get image list from unsplash function if request is successful
         return image_list
@@ -25,7 +25,7 @@ def get_national_park_list(search_query):
     response, error = national_parks.get_parks_data(search_query)
 
     if error:
-        print(f'An error has occured: {error}') # TODO - switch to log instead of print
+        print(f'An error has occurred: {error}') # TODO - switch to log instead of print
     else: 
         parks_list = national_parks.create_park_objects_list(response) # Get parks list from national_parks function if request is successful
         return parks_list
