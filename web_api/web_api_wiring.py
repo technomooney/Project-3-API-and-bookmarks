2aa2feaa92b3b3f9891c17f0760a3090fad4e912
= <nkpratt04@gmail.com>
2023-03-26 16:29:06 -0500

    Add function to call national_parks functions and return list of park objects if no exceptions are raised

--- web_api/web_api_wiring.py
+++ web_api/web_api_wiring.py
@@ -14,3 +14,17 @@ def get_unsplash_image_list():
     else: 
         image_list = unsplash.create_image_object_list(response) # Get image list from unsplash function if request is successful
         return image_list
+
+
+def get_national_park_list(search_query):
+    """This function calls the API request function from national_parks.py.
+    The request function either returns a response or an exception;
+    if there's an error, it will be logged, else, the create park object
+    list function is called and the list of park objects is returned."""
+    response, error = national_parks.get_parks_data(search_query)
+
+    if error:
+        print(f'An error has occured: {error}') # TODO - switch to log instead of print
+    else: 
+        parks_list = national_parks.create_park_objects_list(response) # Get parks list from national_parks function if request is successful
+        return parks_list
