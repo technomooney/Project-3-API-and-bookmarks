9e215c42e4ce10189a71199a73b6e97eb87a04f5
= <nkpratt04@gmail.com>
2023-03-26 19:52:44 -0500

    Add function to get weather forecast data

--- web_api/web_api_wiring.py
+++ web_api/web_api_wiring.py
@@ -29,3 +29,18 @@ def get_national_park_list(search_query):
     else: 
         parks_list = national_parks.create_park_objects_list(response) # Get parks list from national_parks function if request is successful
         return parks_list
+
+
+def get_weather_forecast_object(lat, lon):
+    """This function calls the API request function from open_weather_map.py with 
+    the latitude and longitude of the park entered by the user on the web app.
+    The request function either returns a response or an exception;
+    if there's an error, it will be logged, else, the extract_data
+    function is called and the forecast dictionary is returned."""
+    response, error = open_weather_map.get_api_response(lat, lon)
+
+    if error:
+        print(f'An error has occurred: {error}') # TODO - switch to log instead of print
+    else: 
+        weather_forecast_dict = open_weather_map.extract_data(response) # Get weather forecast ordered dictionary if request is successful
+        return weather_forecast_dict
